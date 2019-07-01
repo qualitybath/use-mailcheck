2c994e03f04b05e582ef50be34947f33798c6093
Michal Tikotzky <michalt@qualitybath.com>
2019-07-01 12:08:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Use-Mailcheck
+
+React component for the [mailcheck](https://github.com/mailcheck/mailcheck) library. Reduce user-misspelled email addresses in your forms by suggesting a right domain when your users misspell it in an email address.
+
+### Installation
+
+```
+yarn add @qb/use-mailcheck
+```
+
+### Usage
+
+```jsx
+import useMailCheck from '@qb/use-mailcheck';
+
+const [inputText, setInputText] = useState(null);
+const suggestion = useMailCheck({ email: inputText });
+
+<div>
+  <input
+    type="email"
+    value={inputText}
+    onChange={e => {
+      setInputText(e.target.value);
+    }}
+  />
+  {suggestion && <div>Did you mean {suggestion.full}?</div>}
+</div>;
+```
+
+- `suggestion`: The suggestion object passed back to you, or null if mailcheck has nothing to suggest. The suggestion object has the following members:
+
+```js
+{
+  address: 'test',        // the address; part before the @ sign
+  domain: 'gmail.com',    // the suggested domain
+  full: 'test@gmail.com'  // the full suggested email
+}
+```
+
+## Thanks
+
+- https://github.com/eligolding/react-mailcheck
+- https://github.com/mailcheck/mailcheck
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import mailcheck from 'mailcheck';
+
+type TSuggestion = {
+  address: string;
+  domain: string;
+  full: string;
+};
+
+type TOptions = {
+  email: string;
+  domainThreshold?: number;
+  topLevelThreshold?: number;
+  domains?: string[];
+  topLevelDomains?: string[];
+  secondLevelDomains?: string[];
+  distanceFunction?(s1: string, s2: string): number;
+};
+
+export default function useMailCheck({
+  email,
+  domainThreshold,
+  topLevelThreshold,
+  domains,
+  topLevelDomains,
+  secondLevelDomains,
+  distanceFunction,
+}: TOptions) {
+  const [suggestion, setSuggestion] = useState<TSuggestion | null>(null);
+
+  useEffect(() => {
+    mailcheck.domainThreshold = domainThreshold || mailcheck.domainThreshold;
+    mailcheck.topLevelThreshold = topLevelThreshold || mailcheck.topLevelThreshold;
+
+    mailcheck.run({
+      email,
+      domains,
+      topLevelDomains,
+      secondLevelDomains,
+      distanceFunction,
+      suggested: setSuggestion,
+      empty: () => {
+        setSuggestion(null);
+      },
+    });
+  }, [
+    email,
+    domainThreshold,
+    topLevelThreshold,
+    setSuggestion,
+    distanceFunction,
+    domains,
+    secondLevelDomains,
+    topLevelDomains,
+  ]);
+
+  return suggestion;
+}
